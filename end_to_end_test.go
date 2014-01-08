95ac5562d46bd6254e592473deb30220d93c05f2
Mat Ryer <mat@stretchr.com>
2014-01-08 15:52:55 -0700

    added multiple buffer size test

--- end_to_end_test.go
+++ end_to_end_test.go
@@ -8,37 +8,50 @@ import (
 	"testing"
 )
 
-func TestMultiReading(t *testing.T) {
+// TestEndToEndAllBuffers tests the concept with an ever-increasing buffer
+// size, starting at 10 and counting up by 10 until it passes 1024.
+func TestEndToEndAllBuffers(t *testing.T) {
 
-	source := "Hello from Stretchr."
-	sourceReader := strings.NewReader(source)
-	caster := New(sourceReader)
+	bufferSize := 0
 
-	r1 := caster.NewReader()
-	r2 := caster.NewReader()
+	for {
 
-	var r1bytes []byte
-	var r2bytes []byte
+		bufferSize = bufferSize + 10
+		source := "Hello from Stretchr."
+		sourceReader := strings.NewReader(source)
+		caster := NewSize(sourceReader, bufferSize)
 
-	// read all in all readers
-	var allread sync.WaitGroup
-	allread.Add(2)
-	go func() {
-		var err error
-		r1bytes, err = ioutil.ReadAll(r1)
-		assert.NoError(t, err)
-		allread.Done()
-	}()
-	go func() {
-		var err error
-		r2bytes, err = ioutil.ReadAll(r2)
-		assert.NoError(t, err)
-		allread.Done()
-	}()
-	allread.Wait() // wait for all readers to finish
+		r1 := caster.NewReader()
+		r2 := caster.NewReader()
 
-	// make sure all bytes are present and correct
-	assert.Equal(t, source, string(r1bytes), "r1bytes")
-	assert.Equal(t, source, string(r2bytes), "r2bytes")
+		var r1bytes []byte
+		var r2bytes []byte
 
+		// read all in all readers
+		var allread sync.WaitGroup
+		allread.Add(2)
+		go func() {
+			var err error
+			r1bytes, err = ioutil.ReadAll(r1)
+			assert.NoError(t, err)
+			allread.Done()
+		}()
+		go func() {
+			var err error
+			r2bytes, err = ioutil.ReadAll(r2)
+			assert.NoError(t, err)
+			allread.Done()
+		}()
+		allread.Wait() // wait for all readers to finish
+
+		// make sure all bytes are present and correct
+		assert.Equal(t, source, string(r1bytes), "r1bytes")
+		assert.Equal(t, source, string(r2bytes), "r2bytes")
+
+		// test with a buffer size of up to 1024
+		if bufferSize > 1024 {
+			break
+		}
+
+	}
 }
